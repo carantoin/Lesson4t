7ece351f785e0c8a1f4d483b21a2daa078584756
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-16 13:41:43 +0200

    Initial commit

---
+++ Lesson4t/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите m:");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    Console.Write("Введите l:");
+    int l = int.Parse(Console.ReadLine());
+    bool x = (n + m > k) && (!(n > k) || (m < l));
+    if (x)
+    {
+        Console.WriteLine("True");
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите правильные данные!");
+}
